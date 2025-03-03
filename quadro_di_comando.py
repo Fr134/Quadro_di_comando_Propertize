5669b1120bb62004374d2ffec1b117d6daf33c32
Fr134 <francesc.daddi@gmail.com>
2025-03-03 21:56:44 +0100

    Update quadro_di_comando.py

--- quadro_di_comando.py
+++ quadro_di_comando.py
@@ -87,7 +87,7 @@ def carica_elaboara_spese(file_path):
     # Elimina le righe in cui:
     # - la colonna "X" è nulla
     # - E la colonna "Y" ha un codice diverso da "57.03.01"
-    file_spese = file_spese[~(file_spese['Importo'].isnull() & (file_spese['Codice'] != '59.01.01'))]
+    file_spese = file_spese[~(file_spese['Importo Totale'].isnull() & (file_spese['Codice'] != '59.01.01'))]
 
     
 
