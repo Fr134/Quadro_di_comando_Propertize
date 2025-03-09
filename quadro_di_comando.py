233566d3f02cc34e6b660c6eb28758fa16e6073d
Fr134 <francesc.daddi@gmail.com>
2025-03-09 01:28:58 +0100

    Update quadro_di_comando.py

--- quadro_di_comando.py
+++ quadro_di_comando.py
@@ -1347,6 +1347,16 @@ def dashboard_proprietari():
             '<span class="info-icon" title="I Ricavi Totali rappresentano la somma complessiva dei ricavi generati dall\'immobile, ottenuti sommando i ricavi da locazione e quelli da servizi aggiuntivi. Questa metrica consente di valutare la performance economica globale dell\'immobile.">ℹ️</span>',
              unsafe_allow_html=True
         )
+            
+        #grafico ad anello 
+        # Sub-layout per centrare il grafico e il dato
+        with col3:
+            totale = kpis["ricavi_totali"]
+            kpi = kpis["marginalità_totale"]
+            grafico_anello = create_donut_chart1(totale, kpi)
+            st.plotly_chart(grafico_anello, use_container_width=False)  # Mantieni larghezza compatta
+        with col3:
+            st.metric("📊 Profitto (€)", f"{kpis['marginalità_totale']:,.2f}")
 
     with col2:
         colonne = ['ricavi_totali', 'commissioni_totali', 'marginalità_totale']
