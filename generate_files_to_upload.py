d7524881ff5bf8a273308b70258d40c4cee36773
Vittorio Carmignani <34647514+carmignanivittorio@users.noreply.github.com>
2025-05-16 12:04:15 +0200

    Update generate_files_to_upload.py

--- generate_files_to_upload.py
+++ generate_files_to_upload.py
@@ -10,7 +10,7 @@ def main():
     """
     company_name = 'company_1'
     create_folder_structure_for_company(company_name)
-    # input("File structure created, if you have uploaded the files, press enter to continue...")
+    input("File structure created, if you have uploaded the files, press enter to continue...")
     stays_df = process_stays_reports(get_raw_report_stays_path(company_name), get_files_to_upload_path(company_name))
     expenses_df = process_expenses_file(f"raw_files/{company_name}/Esempio nota spese.xlsx", f"files_to_upload/{company_name}/expenses.csv")
     kpis = KPIs(stays_df=stays_df, expenses_df=expenses_df)
